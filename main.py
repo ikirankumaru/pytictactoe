679a5c8a6b9f33a9437502b4584eb9ee807c8144
Kiran Kumar <kiranu@somnoware.com>
2024-01-08 10:59:07 +0530

    Determine player markers

--- main.py
+++ main.py
@@ -6,5 +6,22 @@ def display_board(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print(board[1] + '|' + board[2] + '|' + board[3])
 
+def player_input():
+    '''
+    OUTPUT: (Player 1 Marker, Player 2 Marker)
+    '''
+    marker = ''
+
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X','O')
+    
+    return ('O','X')
+
+
+
 test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
+# display_board(test_board)
+# print(player_input())
